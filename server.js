b50b09c7a0059c6309cd93b75b0c520a88674d6e
shabbir <shabbirbadshah128@gmail.com>
2018-02-26 13:07:12 +0530

    aaa

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
-app.use(express(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 
 
