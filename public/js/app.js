d6d378a914fe55f95fcd0efa58c1aa6c5559d9b2
shabbir <shabbirbadshah128@gmail.com>
2018-02-26 17:53:28 +0530

    this is great wok

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+var socket = io();
+
+socket.on('connect', function () {
+    console.log('connected to socket io server');
+})
+
+socket.on('message', function(message){
+          console.log('message is')
+        console.log(message.text)
+      
+})
+
--- server.js
+++ server.js
@@ -2,9 +2,28 @@ var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
+io.on('connection', function(socket) {
+	console.log('user is connected via socket.io')
+    
+    socket.on('message',function(message){
+        console.log('message ' + message.text);
+        
+        socket.broadcast.emit('message', message)
+    })
+    
+    
+    
+    
+    socket.emit('message', {
+        text: 'welcome to the chate application'
+        
+        
+    })
+})
 
 
 
